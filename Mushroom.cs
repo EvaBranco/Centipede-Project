804d8d904f2bd8a977c8289f8c63f9871ec77395
demongirlblaze <demongirlblaze@gmail.com>
2016-12-09 14:18:37 -0600

    Update Mushroom.cs

--- Mushroom.cs
+++ Mushroom.cs
@@ -16,16 +16,23 @@ namespace Centipede
           }
 
         public Rectangle getRect()
-        { return mushR; }
-
-        public void block(playerR, flag)
-        { flag = playerR.Intersects(mushR);
-
-            if (!flag)
-            {
-                playerR.X += 5;
-                playerR.Y += 5;
-            }
+        {
+            return mushR; 
         }
-}
+
+        //Need to be moved to main code
+        //public void block(Rectangle playerR, Bool flag)
+        //{ 
+          //  if(playerR.Intersects(mushR))
+            //   flag=true;
+            //else
+              //  flag=false;
+            
+           // if (!flag)
+            //{
+              //  playerR.X += 5;
+                //playerR.Y += 5;
+            //}
+        }
+    }
 }
