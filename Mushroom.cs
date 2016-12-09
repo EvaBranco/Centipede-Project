0c3bc476628937d3e787e4d626e3a21bff3793ca
kennyKit <makennahenson@icloud.com>
2016-12-09 14:11:18 -0600

    Add files via upload

---
+++ Mushroom.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Centipede
+{
+    class Mushroom
+    {
+        Rectangle mushR;
+
+        public Mushroom()
+           {
+
+            mushR = new Rectangle(450, 450, 10, 10);
+          }
+
+        public Rectangle getRect()
+        { return mushR; }
+
+        public void block(playerR, flag)
+        { flag = playerR.Intersects(mushR);
+
+            if (!flag)
+            {
+                playerR.X += 5;
+                playerR.Y += 5;
+            }
+        }
+}
+}
