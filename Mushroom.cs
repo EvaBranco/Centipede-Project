6306bf9ab20acd2b1f3114b5333633d3b08959b8
demongirlblaze <demongirlblaze@gmail.com>
2016-12-13 13:19:18 -0600

    Update Mushroom.cs

--- Mushroom.cs
+++ Mushroom.cs
@@ -19,20 +19,7 @@ namespace Centipede
         {
             return mushR; 
         }
-
-        //Need to be moved to main code
-        //public void block(Rectangle playerR, Bool flag)
-        //{ 
-          //  if(playerR.Intersects(mushR))
-            //   flag=true;
-            //else
-              //  flag=false;
-            
-           // if (!flag)
-            //{
-              //  playerR.X += 5;
-                //playerR.Y += 5;
-            //}
+       
         public String damaged(Rectangle bullet)
         {
             if(bullet.Intersects(mushR))
