6109a613f6c4cf3db6be3c1b825984694a7dac83
demongirlblaze <demongirlblaze@gmail.com>
2016-12-15 10:47:34 -0600

    Update Centi.cs

--- Centi.cs
+++ Centi.cs
@@ -9,13 +9,21 @@ namespace Centipede
     {
         //instance variables
         private Rectangle centiR;
+        private int screenWidth;
+        private int screenHeight;
         private int cSpeedX = 5;
         private int cSpeedY = 0;
+        private Rectangle cageR;
+        private Rectangle cageL;
 
         //centipede constructor
-        public Centi()
+        public Centi(int height, int width)
         {
             centiR = new Rectangle(0, 0, 50, 50);
+            screenHeight = height;
+            screenWidth = width;
+            cageR = new Rectangle(0, 0, 0, screenHeight);
+            cageL = new Rectangle(800, 0, 0, screenHeight);
         }
 
         public Rectangle getRect()
